6469f70937c24ef5008a17a9c182022b4a3c7b31
Shu <stbook.syu@icloud.com>
2020-09-04 11:33:47 -0700

    add ejs

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const memberController = require('../controllers/members.controller');
-const members = require('../model/Member');
+const memberController = require("../controllers/members.controller");
+const members = require("../model/Member");
 
 //Get ALL members
-router.get('/', memberController.getAllMembers);
+router.get("/", memberController.getAllMembers);
 
 //Get ONE member
-router.get('/:id', memberController.getOneMember);
+router.get("/:id", memberController.getOneMember);
 
 //Create a member
-router.post('/post', memberController.createMember);
+router.post("/post", memberController.createMember);
 
 //Update a member
-router.put('/update/:id', memberController.updateMember);
+router.put("/update/:id", memberController.updateMember);
 
 //Delete a member
-router.delete('/delete/:id', memberController.deleteMember);
+router.delete("/delete/:id", memberController.deleteMember);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 
-const memberRoute = require('./routes/routes');
+const memberRoute = require("./routes/routes");
+const members = require("./model/Member");
+//Template Engine
+app.engine("handlebars", exphbs({ defultLayout: "main" }));
+app.set("view engine", "ejs");
+app.set("views", "views-ejs");
 
 //body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.get('/', (req,res)=> {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Hola with Handlebars", members });
+});
+
 //set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Members API route
-app.use('/api/members', memberRoute);
+app.use("/api/members", memberRoute);
 
 //catch-all-middleware
-app.use((req,res)=> {
-    // res.status(404).send('<h1>Error 404: Page not found</h1>');
-    res.sendFile(path.join(__dirname, 'public', '404.html'));
+app.use((req, res) => {
+  // res.status(404).send('<h1>Error 404: Page not found</h1>');
+  res.sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
